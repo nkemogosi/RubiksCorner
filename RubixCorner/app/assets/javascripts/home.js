fbb2cc19c09e2845a7545b44d3c55d10719a03ed
Nkem Ogosi <ogosinkem@gmail.com>
2019-12-01 18:01:04 +0000

    Revising stopwatch"

--- RubixCorner/app/assets/javascripts/home.js
+++ RubixCorner/app/assets/javascripts/home.js
@@ -3,119 +3,87 @@
 //You can use CoffeeScript in this file: http://coffeescript.org/
 //x = event.which || event.keyCode;
 $(document).ready(function() {
-    var running = false,
-        stopped = true,
-        keyDown = false,
-        dnf = false,
-        plus2 = false;
 
-    var pastTime = $.now();
-    var currentTime = $.now();
-    var millsecs = 0,
-        secs = 0,
-        mins = 0,
-        h1 = $("#timeVal"),
-        delBtn = $(".delBtn"),
-        t, dnfBtn = $(".dnfBtn"),
-        plus2Btn = $(".plus2Btn"),
-        saveBtn = $(".saveBtn");
-    hideBtns();
-    $(document).keypress(function(e) {
-        if(keyDown){
-          return;
-        }
-        if (e.keyCode === 32) {
-          setTimeout(changeColor,100);
-          keyDown=true;
-          if(stopped&&!running){
-            if((mins+secs+millsecs)!=0){
+  var keyDown = false,
+      dnf = false,
+      plus2 = false,
+      timeDelay=1000,
+      pastTime=0,
+      millisecs=0,secs=0,mins=0,
+      h1 = $("#timeVal"),
+      delBtn = $(".delBtn"),
+      t, dnfBtn = $(".dnfBtn"),
+      plus2Btn = $(".plus2Btn"),
+      saveBtn = $(".saveBtn");
+  $(document).keypress(function(e) {
+      if(keyDown){
+        return;
+      }
+    if(e.keyCode ===32){
+      pastTime=$.now();
+      keyDown=true;
+      setTimeout(changeColour,timeDelay);
+    }
+  });
+  $(document).keyup(function(e) {
+      if (e.keyCode === 32) {
+          keyDown=false;
+          $("body").css("background-color","#D8D4D4");
+          if($.now()-pastTime>=timeDelay){
+            if((millisecs+secs+mins)>0){
               save();
+              return;
             }
-            startTimer();
-            stopped=false;
-          }else if(!stopped&&running){
-              startTimer();
+           startStopwatch();
+          }else{
+            clearTimeout(t);
+            showBtns();
           }
-        }
-    });
-    function startTimer(){
-      let currentTime = $.now();
-      if (!running) {
-          pastTime = currentTime;
-      } else if (running) {
-          running = false;
-          stopped = true;
-          showBtns();
-          clearTimeout(t);
       }
-    }
-    $(document).keyup(function(e) {
-        if (e.keyCode === 32) {
-            keyDown=false;
-            let currentTime = $.now();
-
-            if (currentTime - pastTime >= 1000  && !running&&!stopped) {
-                running = true;
-                stopwatch();
-            }
-
-        }
-    });
-    function changeColor(){
-      if(){
-
-      }
-      $("body").css("background-color","green");
-    }
-    function stopwatch() {
-        t = setTimeout(add, 10);
-    }
-
-    function add() {
-        millsecs++;
-        if (millsecs >= 100) {
-            millsecs = 0;
+  });
+  function startStopwatch(){
+   t = setTimeout(add,10)
+  }
+  function add() {
+        millisecs++;
+        if (millisecs >= 100) {
+            millisecs = 0;
             secs++;
             if (secs >= 60) {
                 secs = 0;
                 mins++;
             }
         }
-        let str = (mins ? (mins > 9 ? mins : "0" + mins) : "00") + ":" + (secs ? (secs > 9 ? secs : "0" + secs) : "00") + ":" + (millsecs > 9 ? millsecs : "0" + millsecs);
+        let str = (mins ? (mins > 9 ? mins : "0" + mins) : "00") + ":" + (secs ? (secs > 9 ? secs : "0" + secs) : "00") + ":" + (millisecs > 9 ? millisecs : "0" + millisecs);
+
         h1.html(str);
-        stopwatch();
+        startStopwatch();
     }
-
-    function resetClock() {
-        millsecs = 0;
-        secs  = 0;
-        mins=0;
-        h1.html("00:00:00");
-        running = false;
-        stopped = true;
-        keyDown = false;
-        if(delBtn.is(":visible")){
-          hideBtns();
-        }
-
+  function changeColour(){
+    if(keyDown&&$.now()-pastTime>=timeDelay){
+      $("body").css("background-color","#00ff00");
     }
+  }
+  function resetClock() {
+      millisecs = 0;
+      secs  = 0;
+      mins=0;
+      h1.html("00:00:00");
+      running = false;
+      stopped = true;
+      keyDown = false;
+      if(delBtn.is(":visible")){
+        hideBtns();
+      }
 
-    function hideBtns() {
-        delBtn.hide();
-        plus2Btn.hide();
-        dnfBtn.hide();
-        saveBtn.hide();
-
-    }
-
-    function showBtns() {
-        delBtn.show();
-        plus2Btn.show();
-        dnfBtn.show();
-        saveBtn.show();
-    }
-
-    function save() {
+  }
+  function showBtns() {
+      delBtn.css("visibility","true");
+      plus2Btn.show();
+      dnfBtn.show();
+      saveBtn.show();
+  }
+  function save() {
         if (plus) {
           secs+=2;
         }
@@ -124,10 +92,5 @@ $(document).ready(function() {
         }
         resetClock();
     }
-    delBtn.click(resetClock);
-    saveBtn.click(save);
-  //  dnfBtn.click(resetClock);
-    plus2Btn.click(resetClock);
-
 
 });
